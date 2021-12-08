f7558bd4efbfd798392a9bba7f1d2701a54baef0
adamzermane <adamzermane92@yahoo.com>
2021-12-08 16:05:52 +0100

    restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,16 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:restaurant_id])
+
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
 end
